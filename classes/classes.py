8bf5be663a4eb4e5002636622a3016569fcc894b
Filip Štrba <filipko.strba@gmail.com>
2020-10-18 13:47:16 +0200

    Fixed few typos

--- classes/classes.py
+++ classes/classes.py
@@ -15,7 +15,7 @@ class Zamestnanec:
     #Každý zamestnanec by mal mať: meno, priezvisko, email a plat
 
 
-    #zamestnavatel, navysenie_platu a nastupny_plat sú "class variable", preto ju má každa inštancia Triedy Zamestnanec vlastnosť
+    #zamestnavatel, navysenie_platu a nastupny_plat sú "class variable", preto ju má každa inštancia Triedy Zamestnanec
     zamestnavatel = "SOŠ Handlová" 
     navysenie_platu = 1.01         
     nastupny_plat = 9000
@@ -23,14 +23,14 @@ class Zamestnanec:
     #Funkcie, ktoré sa viažú k tiede sa volajú metódy/"method"
 
 
-    #Ako "constructor" t.j. metóda, ktorá vytvára inštanciu slúži "dunder/magic" metóda __new__(), ktorú netreba dávať do kódu, pokiaľ sa nejedná o zvláštny prípad
+    #Na vytvorenie inštancie Triedy slúži "dunder/magic" metóda __new__(), ktorú netreba dávať do kódu, pokiaľ sa nejedná o zvláštny prípad
 
 
-    #Dunder metóda __init__() slúži na nastavenie vlastností inštancie a je zavolaná vždy automaticky pri vytváraní inštancie;
-    #Ako prvý parameter berie inštanciu triedy, vytvorenú metódou __new__(), zvyklosťou je pomenovávať tento parameter "self";
-    #Toto môže byť zo začiatku metúce, preto uvediem príklad;
-    #Vytvárame inštanciu menom Adam, Adam = Zamestnanec("Adam","Horvát");
-    #Všade, kde sa vyskitne slovo self, môžeme ho nahradiť "Adam";
+    #Dunder metóda __init__() - Contructor slúži na nastavenie vlastností inštancie a je zavolaná vždy automaticky pri vytváraní inštancie
+    #Ako prvý parameter berie inštanciu triedy, vytvorenú metódou __new__(), zvyklosťou je pomenovávať tento parameter "self"
+    #Toto môže byť zo začiatku metúce, preto uvediem príklad
+    #Vytvárame inštanciu menom Adam, Adam = Zamestnanec("Adam","Horvát")
+    #Všade, kde sa vyskitne slovo self, môžeme ho nahradiť "Adam"
     #Takže:
     def __init__(self,meno,priezvisko,plat=nastupny_plat): #self = Adam, meno = "Adam", priezvisko = "Horvát", plat=9000
         self.meno = meno                          #Adam.meno = "Adam"                          
@@ -39,49 +39,49 @@ class Zamestnanec:
         #self.email = f"{meno.lower()}.{priezvisko.lower()}@zssha.edu.sk"
 
 
-    #Dunder metóda __str__() vracia reprezentáciu objektu ako typ "string";
-    #Metóda __str__() je zavolaná vždy, pri voláni funkcii: print(), str();
-    #Ako jediný parameter berie inštanciu triedy;
-    #V tomto prípade metóda __str__() vráti krátke zhrnutie vlastnosti objektu;
+    #Dunder metóda __str__() vracia reprezentáciu objektu ako typ "string"
+    #Metóda __str__() je zavolaná vždy, pri voláni funkcii: print(), str()
+    #Ako jediný parameter berie inštanciu triedy
+    #V tomto prípade metóda __str__() vráti krátke zhrnutie vlastnosti objektu
     def __str__(self):
         return f"{self.meno} {self.priezvisko}, email: {self.email}, plat: {self.plat}"
 
 
-    #Dunder metóda __add__() je zavolaná vždy, keď sa inštancia triedy vyskitne v sčítacej operácii(+) na ľavej strane;
-    #Ako parametre berie 2 inštancie triedy(ščítance), prvá je inštancia na ľavo od + a druhá napravo, inštancia napravo sa podľa zvyklosti nazýva other;
-    #V tomto prípade sa metóda __add__() snaží vráti súčet platov pokiaľ je aj druhý ščítanec inštanciou Triedy Zamestnanec;
-    #Na overenie si, či je druhý ščítanec inštanciou Triedy Zamestnanec použijeme funkciu isinstance(); 
-    #Funkcia isinstance() berei ako parametre inštanciu a Triedu, ktorej má byť prvý parameter inštanciou, vracia "bool" podľa toho, či je prvý parameter skutočne inštanciou;
+    #Dunder metóda __add__() je zavolaná vždy, keď sa inštancia triedy vyskitne v sčítacej operácii(+) na ľavej strane
+    #Ako parametre berie 2 inštancie triedy(ščítance), prvá je inštancia na ľavo od + a druhá napravo, inštancia napravo sa podľa zvyklosti nazýva other
+    #V tomto prípade sa metóda __add__() snaží vrátiť súčet platov pokiaľ je aj druhý ščítanec inštanciou Triedy Zamestnanec
+    #Na overenie si, či je druhý ščítanec inštanciou Triedy Zamestnanec použijeme funkciu isinstance()
+    #Funkcia isinstance() berie ako parametre inštanciu a Triedu, ktorej má byť prvý parameter inštanciou, vracia "bool" podľa toho, či je prvý parameter skutočne inštanciou
     def __add__(self,other):
         if isinstance(other,Zamestnanec):
             return self.plat + other.plat
         return NotImplemented
 
 
-    #Metóda navysenie je "normálnou metódou";
-    #Jej funkciou je navýšiť plat podľa vstupu;
+    #Metóda navysenie je "normálnou metódou"
+    #Jej funkciou je navýšiť plat podľa vstupu
     def navysenie(self,navysenie=navysenie_platu):
         self.plat = int(self.plat * navysenie)
 
 
-    #Decorátor @property zmení metódu na vlastnosť, to znamená, že táto metóda sa zavolá vždy, keď sa volá vlastnost .email;
-    #V tomto prípade metóda email vracia email vo forme "meno.priezvisko@zzsha.edu.sk", pričom meno aj priezvisko su malými písmenami;
+    #Decorátor @property zmení metódu na vlastnosť, to znamená, že táto metóda sa zavolá vždy, keď sa volá vlastnosť .email
+    #V tomto prípade metóda email vracia email vo forme "meno.priezvisko@zzsha.edu.sk", pričom meno aj priezvisko su malými písmenami
     @property
     def email(self):
         return f"{self.meno.lower()}.{self.priezvisko.lower()}@zssha.edu.sk"
 
 
-    #Metóda celé meno má tiež dekorátor @property;
-    #Metóda cele_meno() vracia celé meno vo forme "Meno Priezvisko";
+    #Metóda celé meno má tiež dekorátor @property
+    #Metóda cele_meno() vracia celé meno vo forme "Meno Priezvisko"
     @property
     def cele_meno(self):
         return f"{self.meno} {self.priezvisko}"
 
 
-    #Decorátor @property zmení prvý parameter metódy z inštancie triedy na Triedu;
-    #Keďže "class" je kľúčovým slovom Python-u tak je zvyklosťou tento parameter nazývať cls;
+    #Decorátor @property zmení prvý parameter metódy z inštancie triedy na Triedu
+    #Keďže "class" je kľúčovým slovom Python-u tak je zvyklosťou tento parameter nazývať cls
     #V tomto prípade metóda slovnik() slúži ako alternatýva k funkcii __init__(), s tým, že: 
-    #miesto tradičného vytvorenia inštancie meno_inštancie = Trieda(parameter1,parameter2,...,parametern), sa zavolá táto metóda viď riadok 181;
+    #miesto tradičného vytvorenia inštancie meno_inštancie = Trieda(parameter1,parameter2,...,parametern), sa zavolá táto metóda viď riadok 181
     @classmethod
     def slovnik(cls,dict):
         try:
@@ -90,10 +90,10 @@ class Zamestnanec:
             return cls(dict["meno"],dict["priezvisko"],cls.nastupny_plat)
 
 
-    #Decorátor @staticmethod odstráni prvý povinný parameter metódy(inštacia triedy);
-    #Tieto metódy neinteraktujú priamo z dátamy objektu ale majú nejaké logické spojenie s objektom;
-    #V tomto prípade metóda pracovny_den() vráti "bool" podľa toho, či zadaný deň bol,je alebo bude pracovný;
-    #Modul datetime a jeho vlastnosť .weekday() vracia "int" pričom Pondelok = 0, Utorok = 1, ..., Nedeľa = 6;
+    #Decorátor @staticmethod odstráni prvý povinný parameter metódy(inštacia triedy)
+    #Tieto metódy neinteraktujú priamo z dátamy objektu ale majú nejaké logické spojenie s objektom
+    #V tomto prípade metóda pracovny_den() vráti "bool" podľa toho, či zadaný deň bol,je alebo bude pracovný
+    #Modul datetime a jeho vlastnosť .weekday() vracia "int" pričom Pondelok = 0, Utorok = 1, ..., Nedeľa = 6
     @staticmethod
     def pracovny_den(den):
         if den.weekday() == 5 or den.weekday() == 6:
@@ -101,8 +101,8 @@ class Zamestnanec:
         return True
 
 
-    #.setter dekorátory využivame, pokiaľ chceme využiť metódu s dekorátorom @property v priradení(=);
-    #V tomto prípade @property metóda cele_meno() využíva vlastnosti meno a priezvisko, .setter spôsobý to, že ich budeme môcť jak zavolať tak ja priradiť vo forme "string" "Meno Priezvisko"; 
+    #.setter dekorátory využivame, pokiaľ chceme využiť metódu s dekorátorom @property v priradení(=)
+    #V tomto prípade @property metóda cele_meno() využíva vlastnosti meno a priezvisko, .setter spôsobý to, že ich budeme môcť jak zavolať tak ja priradiť vo forme "string" "Meno Priezvisko"
     #Všeobecný synatx pre setter je @{meno_metódy}.setter
     @cele_meno.setter
     def cele_meno(self, vstup):
@@ -117,18 +117,18 @@ Dedia sa aj vlastnosti aj metódy. Pokiaľ je nejaká vlastnosť alebo metóda z
 class Ucitel(Zamestnanec):
 
 
-    #Trieda Ucitel okrem zákldaných vlastnosti z triedy zamestnanec implemetuje aj vlastnosť "triedny" a "povolanie";
+    #Trieda Ucitel okrem zákldaných vlastnosti z triedy zamestnanec implemetuje aj vlastnosť "triedny" a "povolanie"
 
 
-    #Vlastnosti navysenie_platu a nastupny_plat su zadefinované z novými hodnotami, ktoré budú mať aj inštancie Triedy Ucitel. Vlasnosť povolanie je nová, takže nemá hodnotu na dedenie;
-    #Tieto hodnoty sú preferované voči hodnotám, ktoré boli zdedené;
+    #Vlastnosti navysenie_platu a nastupny_plat su zadefinované z novými hodnotami, ktoré budú mať aj inštancie Triedy Ucitel. Vlasnosť povolanie je nová, takže nemá hodnotu na dedenie
+    #Tieto hodnoty sú preferované voči hodnotám, ktoré boli zdedené
     navysenie_platu = 1.03
     nastupny_plat = 11000
     povolanie = "Učiteľ"
 
 
-    #Aby sme nemuseli opakovať riadky z funkcie __init__() Triedy Zamestnanec môžeme použiť funkciu super(), ktorá reprezentuje rodičovskú funkciu;
-    #Pokiaľ pracujeme s mnohonásobným dedením, je najlepšie namiesto funkcie super() využiť priamo názov rodičovskej funkcie, v tomto prípade Zamestnanec.__init__();
+    #Aby sme nemuseli opakovať riadky z funkcie __init__() Triedy Zamestnanec môžeme použiť funkciu super(), ktorá reprezentuje rodičovskú funkciu
+    #Pokiaľ pracujeme s mnohonásobným dedením, je najlepšie namiesto funkcie super() využiť priamo názov rodičovskej funkcie, v tomto prípade Zamestnanec.__init__()
     def __init__(self,meno,priezvisko,plat=nastupny_plat,triedny=False):
         super().__init__(meno,priezvisko,plat)
         self.triedny = triedny
@@ -137,7 +137,7 @@ class Ucitel(Zamestnanec):
 class Majster(Zamestnanec):
 
 
-    #Trieda Majster okrem základných vlastnosti z Triedy Zamestnanec implementuje aj vlastnosť "ucebna" a "povolanie";
+    #Trieda Majster okrem základných vlastnosti z Triedy Zamestnanec implementuje aj vlastnosť "ucebna" a "povolanie"
 
 
     navysenie_platu = 1.02
@@ -149,7 +149,7 @@ class Majster(Zamestnanec):
         self.ucebna = ucebna
 
 
-#Zákldané inštancovanie inštancie triedy;
+#Zákldané inštancovanie inštancie triedy
 maťko = Zamestnanec("Martin", "Dolník")
 maroš = Majster("Maros","Matejov")
 johny = Ucitel("Jan","Krausko")
